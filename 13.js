7407152ca07d0439ccb84a730183838e2b03fd8e
sasie59 <sasie59@gmail.com>
2021-04-03 23:16:34 +0800

    羅馬數字轉整數

--- 13.js
+++ 13.js
@@ -1,33 +1,25 @@
-import React,{useState} from 'react';
-var obj = {
-  I : 1,
-  V : 5,
-  X : 10,
-  L : 50,
-  C : 100,
-  D : 500,
-  M : 1000,
-};           
-export default function LC13() {
-  const [roman, ] = useState("MCMXCIV");
-  const [int , setRToM] = useState(0);
-
-  const handleClick = () => {
-    // console.warn(obj[roman[0]]);
-    let sum = 0;
-    for(let i = 0 ;i < roman.length; i++) {
-      if(obj[roman[i]] === obj[roman[i + 1]] || obj[roman[i]] > obj[roman[i + 1]]) {
-        sum = sum + obj[roman[i]]
-      } else if(i === roman.length - 1) {
-        setRToM(int + obj[roman[i]]);
-      } else setRToM(int - obj[roman[i]]) 
-    }
-    return int;
+const romanToInt = (s) => {
+  var count = 0;
+  var obj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
   };
-  return (
-    <div>
-      <button onClick={handleClick}>{roman}</button>
-      <div>{int}</div>
-    </div>
-  )
+  for (var i = 0; i < s.length; i++) {
+    if (obj[s[i]] === obj[s[i + 1]] || obj[s[i]] > obj[s[i + 1]]) {
+      count = count + obj[s[i]];
+    } else if (i === s.length - 1) {
+      count = count + obj[s[i]];
+    } else {
+      count = count - obj[s[i]];
+    }
+  }
+  return count;
 };
+console.log(romanToInt("MCMXCIV"));
+console.log(romanToInt("MXCV"));
+console.log(romanToInt("XCV"));
