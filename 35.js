13d50e6d1b3b04fb5900950bd58300532f985f8f
sasie59 <sasie59@gmail.com>
2021-04-04 18:18:25 +0800

    搜索插入位置

---
+++ 35.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = (nums, target) => {
+  for(let i = 0; i <= nums.length - 1 ;i++ ) {
+    if (target < nums[0]) return 0;
+    else if(target > nums[i] && target < nums[i + 1]) return i + 1
+    else if(target > nums[nums.length - 1]) return nums.length
+    else if(nums[i] === target) return i;
+  }
+} 
+
+console.log(searchInsert([1,3,5,6,8],9));
