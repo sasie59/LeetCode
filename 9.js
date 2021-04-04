56a138edf0ac4b4b067637772b4f7757435bbfe2
sasie59 <sasie59@gmail.com>
2021-04-04 10:47:05 +0800

    map & filter 用法

--- 9.js
+++ 9.js
@@ -7,3 +7,12 @@ const isPalindrome = (x) => {
 console.log(isPalindrome('12321'));
 console.log(isPalindrome('-16513210'));
 console.log(isPalindrome('1302031'));
+
+// const add = arr => arr.map(item => item + 3);
+// const filter = arr => arr.filter(item => item.isDone);
+// console.log(add([1,2,3])); 
+// console.log(filter([
+//   {id: 1, isDone: false},
+//   {id: 2, isDone: true},
+//   {id: 3, isDone: true},
+// ])); 
