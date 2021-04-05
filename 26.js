4ef8b53cef604b7d6f6ce519e470f360c9db931d
sasie59 <sasie59@gmail.com>
2021-04-05 22:26:56 +0800

    删除有序數组中的重覆項

---
+++ 26.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  for(let i = 0; i < nums.length;) {
+    if(nums[i] === nums[i + 1]) nums.splice(i + 1, 1);
+    else i++;
+  }  
+  return nums.length;
+};
+
+console.log(removeDuplicates([1,1,2]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
