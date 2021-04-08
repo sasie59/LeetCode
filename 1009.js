3f50fb507b02de2b4cb045341e173dfc1a69df32
sasie59 <sasie59@gmail.com>
2021-04-08 09:04:16 +0800

    十進制整數的反碼

--- 1009.js
+++ 1009.js
@@ -3,11 +3,8 @@ const bitwiseComplement = (N) => {
   console.log(n);
   for(let i = 0; i < n.length; i++) {
     if(n[i] === '1') n[i] = '0';
-    else n[i] = '1'
+    else n[i] = '1';
   };
-  let newNum = parseInt(n, 2);
-  console.log(newNum);
-  return parseInt(n, 2)
-};
-
-console.log(bitwiseComplement(5));
+  return parseInt(n.join(''), 2);
+}
+console.log(bitwiseComplement(8));
