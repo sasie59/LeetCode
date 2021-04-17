75b40c7cf5b706c8cb4ad03bdf58ca5a1df30e5a
sasie59 <sasie59@gmail.com>
2021-04-17 21:42:15 +0800

    Pow(x, n)

---
+++ 50.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  return Math.pow(x, n)
+};
+
+console.log(myPow(2.00000, 10));
+console.log(myPow(2.10000, 3));
+console.log(myPow(2.00000, -2));
