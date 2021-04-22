12c3ea4a495402194e881c082ad60bd4359396e9
sasie59 <sasie59@gmail.com>
2021-04-22 23:47:33 +0800

    有序数组的平方

---
+++ 977.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  let result = nums.map((item) => Math.pow(item, 2));
+  result.sort(function (a, b) {
+    return a - b;
+  });
+  return result;
+};
+
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
+console.log(sortedSquares([-7, -3, 2, 3, 11]));
