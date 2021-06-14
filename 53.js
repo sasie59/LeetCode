478cab75a37890e28068dd442b44a22353797799
sasie59 <sasie59@gmail.com>
2021-06-14 23:55:05 +0800

    最大子序和

---
+++ 53.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+  let sum = 0;
+  let ans = nums[0];
+  for(let i = 0; i < nums.length - 1; i++) {
+    if(sum > 0) {
+      sum+=nums[i];
+    }else {
+      sum = nums[i];
+    }
+    ans = Math.max(ans, sum);
+    // console.log(ans,sum);
+  }
+  return ans;
+};
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
