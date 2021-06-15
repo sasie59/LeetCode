d40dab36c6d87e3d5de546fb7825ff64d2a58ac9
sasie59 <sasie59@gmail.com>
2021-06-15 23:55:54 +0800

    (100題 & 2題) console出來是對的 但是leetcode報錯 若有空幫我看看

--- 2.js
+++ 2.js
@@ -10,11 +10,11 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
+var addTwoNumbers = function (l1, l2) {
   let result = (+l1.reverse().join("") + +l2.reverse().join("")).toString().split("").reverse();
   return result.map(Number);
 };
 
-console.log(addTwoNumbers([2,4,3], [5,6,4]));
+console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
 console.log(addTwoNumbers([0], [0]));
-console.log(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]));
+console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]));
